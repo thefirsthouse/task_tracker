bf7702b0b88091364e7f0f4a7a626e81ca9d3e38
thefirsthouse <vasilijanenko4@gmail.com>
2026-02-15 21:09:11 +0300

    dsadds

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import os
+import datetime
 
 FILE_NAME = "tasks.json"
 
@@ -11,16 +12,53 @@ def load_tasks():
     
     with open(FILE_NAME, "r") as file:
         data = json.load(file)
-        return data
+        return [Task.from_dict(item) for item in data]
+
 
 
 def save_tasks(tasks):
+    items = [item.to_dict() for item in tasks]
     with open(FILE_NAME, "w") as file:
-        json.dump(tasks, file, indent=4)
+        json.dump(items, file, indent=4)
+
+
+class Task:
+    def __init__(self, id, description, status="todo", created_at=None, updated_at=None):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.created_at = created_at
+        self.updated_at = updated_at
+    
+    def to_dict(self):
+        """Put data to dict"""
+        return {
+            "id": self.id,
+            "description": self.description,
+            "status": self.status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+    
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        """Get data from dict"""
+        return cls(
+            id=data["id"],
+            description=data["description"],
+            status=data["status"],
+            created_at=data["created_at"],
+            updated_at=data["updated_at"]
+        )
 
 
 def main():
-    pass
+    tasks = [Task(1, "Test", "todo", str(datetime.datetime.now()), str(datetime.datetime.now()))]
+
+    save_tasks(tasks)
+    svo = load_tasks()
+    print(svo)
 
 if __name__ == "__main__":
     main()
